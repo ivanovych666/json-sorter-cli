62a9c52777fabb95e5c8c5eb7b91ca8455b3255f
Taras Ozarko <tozarko@softjourn.com>
2022-04-09 14:35:57 +0300

    update

--- bin/test.sh
+++ bin/test.sh
@@ -7,4 +7,5 @@ cp test/src/* test/dist/
 
 node dist/ -V
 node dist/ --help
-node dist/ test/dist/1.json test/dist/2.json -ci -r -n
+node dist/ test/dist/**/*.* -ci -r -n -v
+node dist/ test/dist/*.json -ci -r -n
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,53 @@
+#! /usr/bin/env node
+
 import * as fs from 'fs/promises';
 import {program} from 'commander';
 import {jsonSort} from 'json-sorter-lib';
-import {Comparators} from 'json-sorter-lib/dist/comparators';
+import {glob} from 'glob';
+import * as path from 'path';
 
 program.name('<package.name>')
   .description('<package.description>')
   .version('<package.version>');
 
-const log = (...args: any[]): void => {
+const logOn = (...args: any[]): void => {
   console.debug('json-sorter-cli>', ...args);
 };
+
+const logOff = () => null;
 
 program.
   argument('<string...>', 'JSON files to proceed.').
   option('-ci, --case-insensitive', 'For case insensitive sort order.').
   option('-r, --reverse', 'For reverse sort order.').
   option('-n, --natural', 'For natural sort order.').
-  action(async (files, options) => {
-    log(files, options);
+  option('-v, --verbose', 'Show logs.').
+  action(async (patterns, options) => {
+    const {caseInsensitive, reverse, natural, verbose} = options;
+    const log = verbose ? logOn : logOff;
+    log(patterns, options);
+    const filesGlob = await glob.sync(patterns.join(' '));
+    const files = [...patterns, ...filesGlob];
+    const filesSet = new Set<string>();
 
-    const {caseInsensitive, reverse, natural} = options;
-    const comparatorName = [
-      natural ? 'natural' : 'alphabetical',
-      reverse ? 'Reverse' : '',
-      caseInsensitive ? 'Ci' : '',
-      'Sort',
-    ].join('');
-
-    for (let filename of files) {
-      log('start', filename);
-      const input = await fs.readFile(filename, 'utf-8');
-      const output = jsonSort(input, (Comparators as any)[comparatorName]);
-      await fs.writeFile(filename, output, 'utf-8');
-      log('complete', filename);
+    for (let file of files) {
+      const filename = path.resolve(file);
+      try {
+        const stat = await fs.stat(filename);
+        if (stat.isFile()) {
+          filesSet.add(filename);
+        }
+      } catch {}
     }
+
+    const filesArr = Array.from(filesSet.values());
+    for (let file of filesArr) {
+      log(file);
+      const input = await fs.readFile(file, 'utf-8');
+      const output = jsonSort(input, {caseInsensitive, reverse, natural});
+      await fs.writeFile(file, output, 'utf-8');
+    }
+
     log('done');
   });
 
